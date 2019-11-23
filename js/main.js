366fdc47b8fb8603590937e228c29cf9ddc9e435
Oleksandr Lytvyn <oleksandr.lytvyn@student.tuke.sk>
2019-11-23 15:34:56 +0100

    2 task

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    var minh = $(window).height() - ($('.navbar').height() + $('footer').height());
+    $('section').css('min-height', minh + 'px');
+    }
+);
+$('#submit-form').click(function (e){
+    e.preventDefault();
+    $('.form-group input').each(function () {
+        console.log($(this).val());
+    })
+});
