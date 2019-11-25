86e86c34ec15075db94490d5a12e730fda3afbdf
Oleksandr Lytvyn <oleksandr.lytvyn@student.tuke.sk>
2019-11-25 21:53:44 +0100

    registration section added
    registration page should be removed soon

--- js/main.js
+++ js/main.js
@@ -3,13 +3,21 @@ $(document).ready(function(){
     $('section').css('min-height', minh + 'px');
     }
 );
-$('#submit-form').click(function (e){
-    e.preventDefault();
-    $('.form-group input').each(function () {
-        console.log($(this).val());
-    })
-});
+// $('#submit-form').click(function (e){
+//     e.preventDefault();
+//     $('.form-group input').each(function () {
+//         console.log($(this).val());
+//     })
+// });
 
+
+$('#submit-form').click(function(e){
+    var name = $('.form-group input').first().val();
+    var mail = $('.form-group input').eq(1).val();
+
+    $('#regbut').remove();
+    $('.navbar-nav').append('<a>welcome, ' + name + '</a>');
+});
 
 
 // function submitData(){
